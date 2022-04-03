d3996fbd11aed27c02480d1c67d744f5231cece1
marco01123581321 <100161951+marco01123581321@users.noreply.github.com>
2022-04-03 18:15:58 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <h1>
+        Marco Schröder
+      </h1>
+      <div className="landing-container">
+        <h2>
+          Data Engineer
+        </h2>
+        <p className="subtitle">
+          Over the past 2 years I have been a dual student @ Deutsche Telekom and steadily working on new things.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+      <div>
+        <p className="small-header">Who am I?</p>
+        <h2>
+          About me
+        </h2>
+      </div>
+      <div>
+      <p className="small-header">What am I working on?</p>
+        <h2>
+          Projects
+        </h2>
+      </div>
+      <div>
+      <p className="small-header">Do you need a Data Engineer?</p>
+        <h2>
+          Contact me
+        </h2>
+      </div>
+      <footer className="small-header align-center">Made with ♥ by Marco</footer>
     </div>
   );
 }
